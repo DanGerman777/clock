1ad071ab36edbf143a5ac69a14ad99ca6fe7662c
Daniil German <dgerman@ubuntu-1804.udel.edu>
2020-04-26 18:06:42 -0400

    functions added, they do not do anything, only colon prob works

--- display.c
+++ display.c
@@ -3,6 +3,7 @@
 #define RED 0xF800
 #define GREEN 0x07E0
 #define BLUE 0x001F
+//(should prob be in main()  pi_framebuffer_t *fb=getFrameBuffer();
 
 void display_time(int hours, int minutes, int seconds, pi_fraembuffer_t*dev){
 	sense_fb_bitmap_t *bm=fb->bitmap;
@@ -19,4 +20,19 @@ void display_time(int hours, int minutes, int seconds, pi_fraembuffer_t*dev){
     }
 
 }
+void display_colons(pi_framebuffer_t*dev){
+	sense_fb_bitmap_t *bm=fb->bitmap;
+	bm-> pixel[5][1]=WHITE;
+	bm-> pixel[4][1]=WHITE;
+	bm-> pixel[2][1]=WHITE;
+        bm-> pixel[1][1]=WHITE;
+	bm-> pixel[5][3]=WHITE;
+        bm-> pixel[4][3]=WHITE;
+        bm-> pixel[2][3]=WHITE;
+        bm-> pixel[1][3]=WHITE;
+}
+void display_hours(int hours, pi_framebuffer_t*dev){}
 
+void display_minutes(int minutes, pi_framebuffer_t*dev){}
+
+void display_seconds(int seconds, pi_framebuffer_t*dev){}
